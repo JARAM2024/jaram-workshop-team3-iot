48fe907d7bb8878b51b38a042764266c2ad77748
akasha <139549161+junhyeol1@users.noreply.github.com>
2024-07-18 16:02:30 +0900

    Add files via upload

---
+++ send.py
@@ -0,0 +1,57 @@
+import requests
+import json
+
+
+def send_data(temperature, humidity, how_far):
+    url = "http://localhost:8000/update"
+    data = {
+        "temperature": temperature,
+        "humidity": humidity,
+        "is_raining": True,
+        "umbrella_present": False,
+        "door_open": how_far
+    }
+
+    try:
+        response = requests.post(url, json=data)
+        response.raise_for_status()  # 요청 성공 여부 확인
+        print('Data sent successfully:', response.json())
+    except requests.exceptions.RequestException as e:
+        print('Error sending data:', e)
+
+send_data()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def send_data(temp, hum, rain, umb, door):
+    url = "http://localhost:8000/update"
+    data = {
+        "temperature": temp,
+        "humidity": hum,
+        "is_raining": rain,
+       "umbrella_present": umb,
+        "door_open": door
+    }
+
+    try:
+        response = requests.post(url, json=data)
+        response.raise_for_status()  # 요청 성공 여부 확인
+        print('Data sent successfully:', response.json())
+    except requests.exceptions.RequestException as e:
+        print('Error sending data:', e)
+
+send_data()
