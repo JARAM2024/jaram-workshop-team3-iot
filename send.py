e3b5c7645b8194608ba9a0eea579ceced8bff918
wonyoun0809 <wonyoun0809@gmail.com>
2024-07-19 13:37:54 +0900

    a

--- send.py
+++ send.py
@@ -1,16 +1,18 @@
 import requests
+import json
 
 
 def send_data(temperature, humidity, umbrella_present):
-    url = "http://localhost:8000/update"
+    url = "http://192.168.29.117:8000/update"
     data = {
         "temperature": temperature,
         "humidity": humidity,
         "umbrella_present": umbrella_present,
+        "door_open": True
     }
 
     try:
-        response = requests.post(url, json=data)
+        response = requests.post(url, data=json.dumps(data))
         response.raise_for_status()  # 요청 성공 여부 확인
         print('Data sent successfully:', response.json())
     except requests.exceptions.RequestException as e:
